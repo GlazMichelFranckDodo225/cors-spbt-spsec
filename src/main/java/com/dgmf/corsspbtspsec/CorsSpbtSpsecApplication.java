66073dc3871c44663361c86a1efe8c35c330b30c
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-07 08:15:11 +0200

    Project initialization

---
+++ src/main/java/com/dgmf/corsspbtspsec/CorsSpbtSpsecApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.corsspbtspsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsSpbtSpsecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsSpbtSpsecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgmf/corsspbtspsec/CorsSpbtSpsecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.corsspbtspsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsSpbtSpsecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
