ef560344ffc18be8d9d954fdc862795d57523699
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-07 08:42:52 +0200

    Setting up the CoffeeController() constructor and the ArrayList coffeeList

--- src/main/java/com/dgmf/corsspbtspsec/controller/CoffeeController.java
+++ src/main/java/com/dgmf/corsspbtspsec/controller/CoffeeController.java
@@ -1,10 +1,21 @@
 package com.dgmf.corsspbtspsec.controller;
 
+import com.dgmf.corsspbtspsec.model.Coffee;
+import com.dgmf.corsspbtspsec.model.Size;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/coffee")
 public class CoffeeController {
+    private List<Coffee> coffeeList = new ArrayList<>();
 
+    public CoffeeController() {
+        coffeeList.add(new Coffee(1, "Americano", Size.GRANDE));
+        coffeeList.add(new Coffee(2, "Latte", Size.VENTI));
+        coffeeList.add(new Coffee(3, "Caramel Macchiato", Size.TALL));
+    }
 }
