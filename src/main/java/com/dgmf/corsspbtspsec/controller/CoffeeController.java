16c6a32f8ed6fb2beff58bcc8b7c981c09bb8f93
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-07 08:32:32 +0200

    Creating the CoffeeController

---
+++ src/main/java/com/dgmf/corsspbtspsec/controller/CoffeeController.java
@@ -0,0 +1,10 @@
+package com.dgmf.corsspbtspsec.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/coffee")
+public class CoffeeController {
+
+}
