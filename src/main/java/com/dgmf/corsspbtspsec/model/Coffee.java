69b599debd00a567adc6eab765df9fbb7fb9908c
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-07 08:27:22 +0200

    Creation of the record Coffee and the enum Size

---
+++ src/main/java/com/dgmf/corsspbtspsec/model/Coffee.java
@@ -0,0 +1,4 @@
+package com.dgmf.corsspbtspsec.model;
+
+public record Coffee(Integer id, String name, Size size) {
+}
---
+++ src/main/java/com/dgmf/corsspbtspsec/model/Size.java
@@ -0,0 +1,5 @@
+package com.dgmf.corsspbtspsec.model;
+
+public enum Size {
+    SHORT, TALL, GRANDE, VENTI
+}
